0cf9e4d142059d23d5286ab8dd11a536ebb5aa63
KaiEryk <171221979+KaiEryk@users.noreply.github.com>
2026-06-27 19:56:41 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-
 import { defineConfig } from 'astro/config';
 
 /* import data from "./OpenLinks.json"; */
 
-
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://bachhoagiangsen.pages.dev',
+
   vite: {
     plugins: [tailwindcss()]
   }
